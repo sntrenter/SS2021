609bff868df7badc83f05a35f731d07bc5a64c1d
Sam Trenter <sntrenter@gmail.com>
2021-07-02 18:35:47 -0500

    submitted proj1

--- Crytpo/Project1.py
+++ Crytpo/Project1.py
@@ -1,13 +1,18 @@
+#Sam Trenter
+#crypto
+#done with python 3.7.6
+
+
 import re
 import itertools
 import string
 
-#TODO  Write a function that will take in two strings and encrypt the first string with the second string using the Vigenere ciphere.
-#The first string is the plaintext and your function should ignore all non-alphabetic characters (or your code can remove them from the string). All uppercase letters should be treated as lowercase letters (or you can convert the string to all lowercase letters). 
-#The second string is the key and it should be able to be from 1 character long to even longer than the plaintext string itself.
 
 def encrypt(msg = "Hello World!",key = "Default"):
     #remove all but lower alphanumerics
+    if len(key) == 0:
+        print("no key, no encryption")
+        return msg
     msg = re.sub(r'[^a-z ]','',msg.lower())
     key = re.sub(r'[^a-z ]','',key.lower())
     newString = ""
@@ -24,18 +29,21 @@ def encrypt(msg = "Hello World!",key = "Default"):
             num = num - 26
         newString = newString + chr(num)
         keypos += 1
-    print("message:")
-    print(msg)
-    print("key:")
-    print(key)
-    print("encrypted:")
-    print(newString)
+    #print("message:")
+    #print(msg)
+    #print("key:")
+    #print(key)
+    #print("encrypted:")
+    #print(newString)
     return newString
     
 
 #TODO Then write a function that, given two strings, decrypts the first string using the second string. Same conditions should apply as before.
 def decrypt(msg = "kiqli hhupi", key = "default"):
     #remove all but lower alphanumerics
+    if len(key) == 0:
+        print("no key, no decryption")
+        return msg
     msg = re.sub(r'[^a-z ]','',msg.lower())
     key = re.sub(r'[^a-z ]','',key.lower())
     newString = ""
@@ -62,6 +70,9 @@ def decrypt(msg = "kiqli hhupi", key = "default"):
 #TODO Once these functions are completed, have your main body prompt the user for a string to encode, then a key string, then perform the encryption of the plaintext and output the ciphertest.. Then decrypt it and show the resulting plaintext.
 
 def decipher(msg = "",sub = "",keysize = 5):
+    if keysize == "" or msg == "" or sub == "":
+        print("give valid variables")
+    keysize = int(keysize)
     alphas = string.ascii_lowercase
     i = 1
     print("trying to dycrypt")
@@ -76,15 +87,28 @@ def decipher(msg = "",sub = "",keysize = 5):
                 print("possible solution:",k)
                 print("decoded msg: ",decrypt(msg,k))
 
-
+        if input("continue?(y/n):") == "n":
+            break
         i+=1
 
 
 
 
 
-def main():
-    encrypt()
-    decrypt()
-    decipher("boil ma h vxvg sogk kppaiz p htxm ahx yv p htxm ahx yv p htxm ahx yv","cipher")
+def main():    
+    print("encrypt")
+    val1 = input("enter message to encrypt: ")
+    val2 = input("enter key to encrypt with: ")
+    val3 = encrypt(val1,val2)
+    print("the encrypted msg: ", val3)
+    print("decrypting")
+    print("decrypted message: ",decrypt(val3,val2))
+
+    print("decipher")
+    msg = input("give cipher text: ")
+    submsg = input("give substring that should exist: ")
+    keysize = input("what is the max keysize: ")
+    decipher(msg,submsg,keysize)
+
+
 main()
