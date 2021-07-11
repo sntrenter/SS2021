ac5ba33f2483c1ff97a0792bfbeeafa1b027f1a2
Sam Trenter <sntrenter@gmail.com>
2021-07-11 12:46:49 -0500

    remove print statements

--- 4220/Pokepedia/Pokepedia/Details/PokemonDetailsViewController.swift
+++ 4220/Pokepedia/Pokepedia/Details/PokemonDetailsViewController.swift
@@ -21,27 +21,16 @@ final class PokemonDetailsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(pokémon?.name ?? "default")
         nameLabel.text = "name: " +  (pokémon?.name ?? "Name doesn't exist!")
-        print(pokémon?.id ?? 0)
         idLabel.text = "ID: " + String(pokémon?.id ?? 0)
-        print(pokémon?.baseExperience ?? 0)
         baseEXPLabel.text =  "BaseEXP: " + String(pokémon?.baseExperience ?? 0)
-        print(pokémon?.height ?? 0)
         heightLabel.text = "Height: " + String(pokémon?.height ?? 0)
-        print(pokémon?.weight ?? 0)
         weightlabel.text = "Weight: " + String(pokémon?.weight ?? 0)
-        print(pokémon?.species?.name ?? "No Species?")
         speciesLabel.text = "Species: " + (pokémon?.species?.name ?? "No Species?")
-        print(pokémon?.types?.first?.type?.name ?? "No Type?")
         typeLabel.text = "Type: " + (pokémon?.types?.first?.type?.name ?? "No Type?")
-        print(pokémon.abilities?.first?.ability?.name ?? "No Abilty?")
         abiltiyLabel.text = "Ability: " + (pokémon.abilities?.first?.ability?.name ?? "No Abilty?")
-        print(pokémon.moves?.first?.move?.name ?? "No Move?" )
         moveLabel.text = "Move: " + (pokémon.moves?.first?.move?.name ?? "No Move?")
-        print(pokémon.heldItems?.first?.item?.name ?? "No Item?")
         heldItemsLabel.text = "Held Item: " + (pokémon.heldItems?.first?.item?.name ?? "No Item?")
-        //print(pokémon.stats?.first?.stat?.name ?? "No Stat?")
         
     }
 }
